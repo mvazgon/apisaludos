57fcdde1cb0b972c2ce0055534e9998df7db1bc3
Manuel Vazquez <mvazquez@externos.itnow.es>
2023-11-21 15:28:27 +0100

    importación del proyecto al repositorio

---
+++ src/main/java/org/mvazquez/api/apisaludos/ApiSaludosApplication.java
@@ -0,0 +1,13 @@
+package org.mvazquez.api.apisaludos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSaludosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSaludosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mvazquez/api/apisaludos/ApiSaludosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mvazquez.api.apisaludos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSaludosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
